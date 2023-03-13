8717672443aa0837ca111b599a728faec5cbf0d1
cnchen.pu <31865876+cnchenpu@users.noreply.github.com>
2023-03-13 09:34:58 +0800

    Update lab1-3_ctypes.c

--- lab1/lab1-3_ctypes.c
+++ lab1/lab1-3_ctypes.c
@@ -1,5 +1,5 @@
 /* Lab 1-3:
- * ctypes.c  */
+ *  * ctypes.c  */
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -7,23 +7,23 @@
 
 int main()
 {
-    printf("char: %d bytes\n", sizeof(char));
-    printf("unsigned char: %d bytes\n", sizeof(unsigned char));
-    printf("signed char: %d bytes\n", sizeof(signed char));
-    printf("int: %d bytes\n", sizeof(int));
-    printf("unsigned int: %d bytes\n", sizeof(unsigned int));
-    printf("signed int: %d bytes\n", sizeof(signed int));
-    printf("short int: %d bytes\n", sizeof(short int));
-    printf("unsigned short: %d bytes\n", sizeof(unsigned short));
-    printf("signed short: %d bytes\n", sizeof(signed short));
-    printf("long int: %d bytes\n", sizeof(long int));
-    printf("unsigned long: %d bytes\n", sizeof(unsigned long));
-    printf("signed long: %d bytes\n", sizeof(signed long));
-    printf("long long: %d bytes\n", sizeof(long long));
-    printf("unsigned long long: %d bytes\n", sizeof(unsigned long long));
-    printf("float: %d bytes\n", sizeof(float));
-    printf("double: %d bytes\n", sizeof(double));
-    printf("bool: %d bytes\n", sizeof(bool));
+    printf("char\t\t\t: %d bytes\n", sizeof(char));
+    printf("unsigned char\t\t: %d bytes\n", sizeof(unsigned char));
+    printf("signed char\t\t: %d bytes\n", sizeof(signed char));
+    printf("int\t\t\t: %d bytes\n", sizeof(int));
+    printf("unsigned int\t\t: %d bytes\n", sizeof(unsigned int));
+    printf("signed int\t\t: %d bytes\n", sizeof(signed int));
+    printf("short int\t\t: %d bytes\n", sizeof(short int));
+    printf("unsigned short\t\t: %d bytes\n", sizeof(unsigned short));
+    printf("signed short\t\t: %d bytes\n", sizeof(signed short));
+    printf("long int\t\t: %d bytes\n", sizeof(long int));
+    printf("unsigned long\t\t: %d bytes\n", sizeof(unsigned long));
+    printf("signed long\t\t: %d bytes\n", sizeof(signed long));
+    printf("long long\t\t: %d bytes\n", sizeof(long long));
+    printf("unsigned long long\t: %d bytes\n", sizeof(unsigned long long));
+    printf("float\t\t\t: %d bytes\n", sizeof(float));
+    printf("double\t\t\t: %d bytes\n", sizeof(double));
+    printf("bool\t\t\t: %d bytes\n", sizeof(bool));
 
     return 0;
 }
